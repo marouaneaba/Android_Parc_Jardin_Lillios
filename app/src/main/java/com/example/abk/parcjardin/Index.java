600475e0e4e14271141ad0b5a1dcb93979021269
marouaneaba <mar.abakarim@gmail.com>
2018-01-24 02:02:10 +0100

    regler allumage de l'application , gérer les intent

--- app/src/main/java/com/example/abk/parcjardin/Index.java
+++ app/src/main/java/com/example/abk/parcjardin/Index.java
@@ -1,7 +1,9 @@
 package com.example.abk.parcjardin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by abk on 19/01/2018.
@@ -9,10 +11,20 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Index extends AppCompatActivity {
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.index);
+    }
+
+    public void Go(View v){
+        Intent intent = new Intent(Index.this, MainActivity.class);
+        startActivity(intent);
     }
 
 }
